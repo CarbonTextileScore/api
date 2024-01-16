c52dab2192d8feb5b8b85972ca1eac62ed9e897c
Steimetz <tangui.steimetz@ecole.ensicaen.fr>
2024-01-16 17:32:14 +0100

    Add new entity
    Update SQL script for new table CityRetribution
    Create related entity

---
+++ src/main/java/fr/carbon/textile/score/api/database/entity/quota_information/CityRetributionEntity.java
@@ -0,0 +1,67 @@
+package fr.carbon.textile.score.api.database.entity.quota_information;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity(name = "CityRetribution")
+@Table(name = "`CityRetribution`", schema = "`QuotaInformation`", catalog = "postgres")
+public class CityRetributionEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "`id`")
+    private Integer _id;
+    @Basic
+    @Column(name = "`Retribution`", nullable = false)
+    private String _retribution;
+    @Basic
+    @Column(name = "`TriggerPercentage`", nullable = false)
+    private int _triggerPercentage;
+
+    public CityRetributionEntity() {
+    }
+
+    public CityRetributionEntity(String retribution, int triggerPercentage) {
+        this._retribution = retribution;
+        this._triggerPercentage = triggerPercentage;
+    }
+
+    public Integer getId() {
+        return _id;
+    }
+
+    public void setId(Integer id) {
+        this._id = id;
+    }
+
+    public String getRetribution() {
+        return _retribution;
+    }
+
+    public void setRetribution(String retribution) {
+        this._retribution = retribution;
+    }
+
+    public int getTriggerPercentage() {
+        return _triggerPercentage;
+    }
+
+    public void setTriggerPercentage(int triggerPercentage) {
+        this._triggerPercentage = triggerPercentage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CityRetributionEntity that = (CityRetributionEntity) o;
+        return _triggerPercentage == that._triggerPercentage &&
+                Objects.equals(_id, that._id) &&
+                Objects.equals(_retribution, that._retribution);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_id, _retribution, _triggerPercentage);
+    }
+}
