4c65a74b7bb0e095b02f19046d4fa2ee2e095813
Steimetz <tangui.steimetz@ecole.ensicaen.fr>
2024-01-17 14:07:17 +0100

    Security update
    Update security and classes from previous merge update.
    Remove temporary repository and migrate to the correct one

--- src/main/java/fr/carbon/textile/score/api/repository/user/information/AuthorityRepository.java
+++ src/main/java/fr/carbon/textile/score/api/repository/user/information/AuthorityRepository.java
@@ -1,9 +1,20 @@
 package fr.carbon.textile.score.api.repository.user.information;
 
 import fr.carbon.textile.score.api.database.entity.user.information.AuthorityEntity;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AuthorityRepository extends JpaRepository<AuthorityEntity, Long> {
+    @Query(
+            "SELECT authority FROM Authority authority WHERE authority._username = :username"
+    )
+    Optional<AuthorityEntity> queryByUsername(
+            @NotBlank @Param(value = "username") String username
+    );
 }
