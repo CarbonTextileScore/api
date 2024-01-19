f254bd6bf6753187cd8ea67295169d06bc1c3c9b
EniXoo <lebarbanchon.valentin@gmail.com>
2024-01-19 16:56:41 +0100

    main  - /invoice POST Fixed

--- src/main/java/fr/carbon/textile/score/api/security/CorsConfig.java
+++ src/main/java/fr/carbon/textile/score/api/security/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "http://192.93.212.213:8443")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders(CorsConfiguration.ALL)
                 .exposedHeaders("Token")
@@ -26,6 +26,7 @@ public class CorsConfig implements WebMvcConfigurer {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("http://localhost:4200");
+        corsConfiguration.addAllowedOrigin("http://192.93.212.213:8443");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("GET");
         corsConfiguration.addAllowedMethod("POST");
